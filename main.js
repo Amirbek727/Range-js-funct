2d5e256fa0d56ad344bdf7fe011a9dcbf4e639bc
Amirbek727 <amirika738@gmail.com>
2024-09-09 22:10:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// let son = 3;
+
+//  while (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+
+
+
+// let son = 3;
+
+//  Range (son < 100 ) 
+//    if () {
+
+//    } else {
+      
+//    }
+//     console.log(son); 
+//     son = son + 3;
+
+
+function range(a, b) {
+  while (a <=b ) {
+    console.log(a);
+    a = a + 2;
+  }
+}
+range(50, 100);
